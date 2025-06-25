a61923a5305dd3e0b1841ea54d2eebfa15e08b29
Alan Panayotov <alanpanayotov@gmail.com>
2025-06-25 13:21:04 +0100

    feat: enable cloudflare-ddns

--- hosts/ein/default.nix
+++ hosts/ein/default.nix
@@ -28,18 +28,18 @@
     supportedFilesystems = ["ntfs" "vfat" "ext4" "lvm" "xfs"];
   };
 
-  #  services.cloudflare-dyndns = {
-  #    enable = true;
-  #    apiTokenFile = config.sops.secrets.cloudflare-api-token.path;
-  #    domains = [
-  #      "ein"
-  #    ];
-  #    proxied = true;
-  #  };
+  services.cloudflare-dyndns = {
+    enable = true;
+    apiTokenFile = config.sops.secrets.cloudflare-api-token.path;
+    domains = [
+      "ein"
+    ];
+    proxied = true;
+  };
 
-  # sops.secrets.cloudflare-api-token = {
-  #   sopsFile = ./secrets.yaml;
-  # };
+  sops.secrets.cloudflare-api-token = {
+    sopsFile = ./secrets.yaml;
+  };
 
   services.openssh = {
     enable = true;
@@ -52,7 +52,7 @@
     hostName = "ein";
     firewall.allowedTCPPorts = [80 443 22];
   };
-  users.users.root.password = "cock";
+  users.users.root.password = "";
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKKUWJ2oqT2pcU1LR8iOG03FXh8rBsUg8yNfEi13yofq alan@gamer-think"
     "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC+9vPgSsCWRTtVmS+8MhBz/aKKPFPZlZc2iZ4Xf7oJa4LvzScMlh/O9mlQepGIuVUnGYB0AJ8CP3jGw/p8Yn2QGD7YE+w4xf4c03fpELFxeOMSZEeBEjXb0+tkLWoWY28mVFpmr2zt/Vj1uzvR1hxoiGV20VHViSRxzIrmMGYT0njhcdVfu5JcfS1fdXVrSbF6SwAut/49O8Tb5sp83tNKc9S/nR0ReQMwMReOAOkB4MemB05eqjIwbwall38ualf/vF395ObO1LKLiU1DjILfm33Oa57UAHpudoK3GzUZdqaAJeL1mTaH5o9Lvy9cJcTN+RHQhwv2hCjzUR1swMfrGSuNNIpy22ghSf8bOU1dyvtljMax6i+WKPUzeRKVI9bWCcjZZFd0OUA2MfqsWPX+ER29uaWcssHb75Mv1oA3R6FgJ4LokghxUUYZZfjjUZlLpRkgdZQvr10ZKiG1gjzgw0E+un9BT2Ts0g6smtN3I1s525iG3eY33fBt0UU8RJ0= alan@zwei-pc"

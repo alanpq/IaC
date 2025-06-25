f4e1402fabf0acd4682edf01a039c6032faebefa
Alan Panayotov <alanpanayotov@gmail.com>
2025-06-25 12:59:06 +0100

    fix: remove vm stuff

--- flake.nix
+++ flake.nix
@@ -2,8 +2,6 @@
   description = "Infra flake";
 
   nixConfig = {
-    extra-substituters = ["https://microvm.cachix.org"];
-    extra-trusted-public-keys = ["microvm.cachix.org-1:oXnBc6hRE3eX5rSYdRyMYXnfzcCxC7yKPTbZXALsqys="];
   };
 
   inputs = {
@@ -18,31 +16,21 @@
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    microvm = {
-      url = "github:astro/microvm.nix";
-    };
   };
 
   outputs = {
     self,
     nixpkgs,
-    microvm,
     disko,
     sops-nix,
     ...
   }: let
     system = "x86_64-linux";
   in {
-    packages.${system} = {
-      ein = self.nixosConfigurations.ein.config.microvm.declaredRunner;
-    };
-
     nixosConfigurations = {
       ein = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          microvm.nixosModules.microvm
           sops-nix.nixosModules.sops
           disko.nixosModules.disko
           ./hosts/ein
--- hosts/ein/default.nix
+++ hosts/ein/default.nix
@@ -65,48 +65,5 @@
     pkgs.nss.tools # needed for caddy self signed certs?
   ];
 
-  virtualisation.vmVariantWithDisko = {
-    virtualisation = {
-      forwardPorts = [
-        {
-          from = "host";
-          host.port = 2222;
-          guest.port = 22;
-        }
-      ];
-      sharedDirectories = {
-        my-share = {
-          source = "/tmp/tmp.1b4nwgTkDb/etc/ssh/";
-          target = "/etc/ssh";
-        };
-      };
-    };
-  };
-
-  microvm = {
-    volumes = [
-      {
-        mountPoint = "/var";
-        image = "var.img";
-        size = 256;
-      }
-    ];
-    shares = [
-      {
-        # use proto = "virtiofs" for MicroVMs that are started by systemd
-        proto = "9p";
-        tag = "ro-store";
-        # a host's /nix/store will be picked up so that no
-        # squashfs/erofs will be built for it.
-        source = "/nix/store";
-        mountPoint = "/nix/.ro-store";
-      }
-    ];
-
-    # "qemu" has 9p built-in!
-    hypervisor = "qemu";
-    socket = "control.socket";
-  };
-
   system.stateVersion = "25.05";
 }
